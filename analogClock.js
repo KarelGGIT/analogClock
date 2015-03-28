e099de5338661be5ed6551f671a789cc2c910bd9
karel <karel.geiregat@student.kuleuven.be>
2015-03-28 16:42:26 +0100

    adapted layout js file

--- analogClock.js
+++ analogClock.js
@@ -1,9 +1,29 @@
 /*
 * Clock class to show an analogue clock in JavaScript only using HTML5 canvas element.
 *
-* 						!!! WORK IN PROGRESS !!!
+* The MIT License (MIT)
+* 
+* Copyright (c) 2015 KarelG
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+* 
 * version 1.0.2
-* - improved colors, "look and feel" and smoothness seconde arrow
 */
 
 var analogClock = function(canvasID, options)
@@ -35,6 +55,7 @@ var analogClock = function(canvasID, options)
 	var _clock = null;
 	var _center = null;
 
+	// obj to hold size
 	var ClockLayout = function(radius) {
 		this.RADIUS = radius;
 		this.HOUR = radius * 0.7;
@@ -42,46 +63,45 @@ var analogClock = function(canvasID, options)
 		this.SECOND = radius * 0.95;
 	};
 
-	/*
-	coordinate holder
-	*/
+	// obj to hold coordinate
 	var Coordinate = function(x, y) {
 		this.x = x;
 		this.y = y;
 	};
 
+	/*
+	* Time object with functions to handle it in clock display
+	*/
 	var Time = function(hour, minute, second, millisecond) {
 		this.hour = hour;
 		this.minute = minute;
 		this.second = second;
 		this.ms = millisecond;
 	};
-
+	var getTime = function() {
+		var d = new Date();
+		return new Time(d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds());
+	};
 	Time.prototype.getCoordinate = function(angleInDegree, typeClock) {
 		var a = transformToRadian(angleInDegree);
 		var x = _center.x + (Math.sin(a) * typeClock);
 		var y = _center.y - (Math.cos(a) * typeClock);
 		return new Coordinate(x, y);
 	};
-
 	Time.prototype.getHourEndPoint = function() {
 		var angleInDegree = (1/2) * (60 * (this.hour % 12) + this.minute);
 		return this.getCoordinate(angleInDegree, _clock.HOUR);
 	};
-
 	Time.prototype.getMinuteEndPoint = function() {
 		var angleInDegree = this.minute * 6;
 		return this.getCoordinate(angleInDegree, _clock.MINUTE);
 	};
-
 	Time.prototype.getSecondEndPoint = function() {
 		var angleInDegree = (this.second * 1000 + this.ms) * (6 / 1000);
 		return this.getCoordinate(angleInDegree, _clock.SECOND);
 	};
 
-	/*
-	* === canvas core drawing functions
-	*/
+	// canvas core drawing functions
 	var drawLine = function(xStart, yStart, xEnd, yEnd, lineWidth, color) {
 		_ctx.lineWidth = (lineWidth || DEFAULTWIDTH);
 		_ctx.strokeStyle = (color || "#000000");
@@ -91,14 +111,6 @@ var analogClock = function(canvasID, options)
 		_ctx.stroke();
 	};
 
-	var getTime = function() {
-		var d = new Date();
-		return new Time(d.getHours(), d.getMinutes(), d.getSeconds(), d.getMilliseconds());
-	};
-
-	var clearCanvas = function() {
-		_ctx.clearRect(0, 0, _canvas.width, _canvas.height);
-	};
 	/*
 	functions to draw clock
 	*/
@@ -122,7 +134,10 @@ var analogClock = function(canvasID, options)
 		// draw seconde
 		drawLine(_center.x, _center.y, coS.x, coS.y, 2, secondNeedleColor);
 	};
-
+	// clear canvas
+	var clearCanvas = function() {
+		_ctx.clearRect(0, 0, _canvas.width, _canvas.height);
+	};
 	var drawLayout = function() {
 		// draw center point
 		_ctx.lineWidth = 1;
@@ -184,7 +199,7 @@ var analogClock = function(canvasID, options)
 		drawClock();
 	};
 
-	// init
+	// init steps
 	var canvas = document.getElementById(canvasID);
 	if (canvas != null)
 		initializeClock(canvas);
